bc4e8783d70da7b08b556dd3bdd8318554f0da9d
Kaviilee <jiaweilee95@126.com>
2023-05-01 20:55:57 +0800

    fix: talent recommend

--- src/service/talent_recommend.js
+++ src/service/talent_recommend.js
@@ -2,7 +2,7 @@ import { $cms } from "@jx3box/jx3box-common/js/https_v2.js";
 
 // 获取奇穴推荐
 function getTalentRecommendList(params) {
-    return $cms().get("/api/cms//app/talent_default", {
+    return $cms().get("/api/cms/app/talent_default", {
         params: params,
     });
 }
