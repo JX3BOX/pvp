0e9590f565f38ec23361af117f49b9c7eb07b5d1
Kaviilee <jiaweilee95@126.com>
2023-10-17 14:44:31 +0800

    chore: update service

--- src/service/rank.js
+++ src/service/rank.js
@@ -3,7 +3,7 @@ import { $cms } from "@jx3box/jx3box-common/js/https_v2";
 // 创建热门门派排行榜
 function createRankItem(data) {
     return $cms()
-        .post(`/api/cms/bps/pvp-jjc-rank`, data)
+        .post(`/api/cms/pvp/jjc-rank`, data)
         .then((res) => {
             return res.data;
         });
@@ -12,7 +12,7 @@ function createRankItem(data) {
 // 获取热门门派排行榜
 async function getRankList(params) {
     return $cms()
-        .get(`/api/cms/bps/pvp-jjc-rank`, {
+        .get(`/api/cms/pvp/jjc-rank`, {
             params: params,
         })
         .then((res) => {
@@ -22,7 +22,7 @@ async function getRankList(params) {
 // 更新排行榜
 async function putRankList(id, data) {
     return $cms()
-        .put(`/api/cms/bps/pvp-jjc-rank/${id}`, data)
+        .put(`/api/cms/pvp/jjc-rank/${id}`, data)
         .then((res) => {
             return res.data;
         });
@@ -30,7 +30,7 @@ async function putRankList(id, data) {
 // 删除排行榜
 async function delRankList(id) {
     return $cms()
-        .delete(`/api/cms/bps/pvp-jjc-rank/${id}`)
+        .delete(`/api/cms/pvp/jjc-rank/${id}`)
         .then((res) => {
             return res.data;
         });
