c8c89604581b979b8918488e5e72faf220e8bf8c
iRuxu <rx6@qq.com>
2023-11-16 23:48:49 +0800

    feat:video suffix

--- src/components/trick/CompetitiveTrickItem.vue
+++ src/components/trick/CompetitiveTrickItem.vue
@@ -7,9 +7,10 @@
                 <span class="u-marks" v-if="data.mark && data.mark.length">
                     <i v-for="mark in data.mark" class="u-mark" :key="mark">{{ showMark(mark) }}</i>
                 </span>
-                <span class="u-tv" v-if="data.include_video"
-                    ><img svg-inline class="u-tv-icon" src="@/assets/img/tv.svg" />视频</span
-                >
+                <span class="u-tv" v-if="data.include_video">
+                    <img class="u-tv-icon" src="@/assets/img/video.svg" />
+                    <!-- <span class="u-tv-text">视频</span> -->
+                </span>
             </div>
             <div class="m-trick-item__thx">
                 <SimpleThx
